8a70f34e2463ced9ea68a6265fac2edd4aa2dd41
samir ibrahim <114286708+samir419@users.noreply.github.com>
2025-03-13 17:19:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 app.get('/data', (req, res) => {
     res.json({ message: "Hello, world!", status: "success" });
+});
+
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
 
 app.listen(port, () => {
